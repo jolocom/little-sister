524eaacd2f22778fec51a47bcf07b52f30cb6d0b
Sean Li <li.saifahn@gmail.com>
2019-05-20 15:57:55 +0200

    [WIP] Separate documentCard out from documents

---
+++ src/ui/home/components/documentCard.tsx
@@ -0,0 +1,116 @@
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+import { JolocomTheme } from 'src/styles/jolocom-theme'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+
+export interface Document {
+  details: {
+    type: string
+    id_number: string
+    [key: string]: string
+  }
+  valid_until: Date | undefined
+  issuer: string
+}
+
+interface DocumentCardProps {
+  document: Document
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    width: '100%',
+  },
+  card: {
+    height: 176,
+    backgroundColor: JolocomTheme.primaryColorSand,
+    borderColor: 'rgb(233, 233, 233)',
+    borderWidth: 1,
+    borderRadius: 10,
+    width: 276,
+    paddingVertical: 16,
+  },
+  documentType: {
+    paddingHorizontal: 15,
+    fontSize: 28,
+    fontFamily: JolocomTheme.contentFontFamily,
+  },
+  documentNumber: {
+    paddingHorizontal: 15,
+    fontSize: 17,
+    fontFamily: JolocomTheme.contentFontFamily,
+  },
+  validityContainer: {
+    flexDirection: 'row',
+    color: 'red',
+    marginTop: 'auto',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    width: '100%',
+    height: 50,
+    paddingHorizontal: 15,
+  },
+  validityText: {
+    marginLeft: 5,
+    fontSize: 17,
+  },
+})
+
+export const DocumentCard: React.SFC<DocumentCardProps> = (
+  props,
+): JSX.Element => (
+  <View style={styles.container}>
+    <View style={styles.card}>
+      <Text style={styles.documentType}>{props.document.details.type}</Text>
+      <Text style={styles.documentNumber}>
+        {props.document.details.id_number}
+      </Text>
+      <View style={styles.validityContainer}>
+        <Icon size={17} name="check-all" />
+        {props.document.valid_until && (
+          <Text style={styles.validityText}>
+            Valid until {props.document.valid_until.toLocaleDateString('en-GB')}
+          </Text>
+        )}
+      </View>
+    </View>
+  </View>
+)
+
+const collapsedDocCardStyles = StyleSheet.create({
+  container: {
+    paddingTop: 20,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  card: {
+    backgroundColor: JolocomTheme.primaryColorWhite,
+    width: 101,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'grey',
+    height: 64,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  icon: {
+    backgroundColor: JolocomTheme.primaryColorGrey,
+    width: 42,
+    height: 42,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
+
+export const CollapsedDocumentCard = (): JSX.Element => (
+  <View style={collapsedDocCardStyles.container}>
+    <View style={collapsedDocCardStyles.card}>
+      <View style={collapsedDocCardStyles.icon}>
+        <Icon size={20} name="checkbox-multiple-blank" />
+      </View>
+    </View>
+  </View>
+)
--- src/ui/home/components/interactions.tsx
+++ src/ui/home/components/interactions.tsx
@@ -8,29 +8,59 @@ import {
   NativeScrollEvent,
   NativeSyntheticEvent,
 } from 'react-native'
+import {
+  CollapsedDocumentCard,
+  Document,
+  DocumentCard,
+} from 'src/ui/home/components/documentCard'
 import { JolocomTheme } from 'src/styles/jolocom-theme'
 import I18n from 'src/locales/i18n'
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 const Carousel = require('react-native-snap-carousel').default
 const Pagination = require('react-native-snap-carousel').Pagination
 
 interface Props {}
 
-interface Document {
-  details: {
-    type: string
-    id_number: string
-    [key: string]: string
-  }
-  valid_until: Date | undefined
-  issuer: string
-}
-
-interface DocumentCardProps {
-  document: Document
-}
-
 const demoDocuments: Document[] = [
+  // {
+  //   issuer: 'did:jolo:aa1bb2cc3dd4',
+  //   details: {
+  //     id_number: 'T3ST0001',
+  //     type: 'A-kaart',
+  //   },
+  //   valid_until: new Date(Date.parse('2020-04-29')),
+  // },
+  // {
+  //   issuer: 'did:jolo:aa1bb2cc3dd4',
+  //   details: {
+  //     id_number: 'T3ST0001',
+  //     type: 'A-kaart',
+  //   },
+  //   valid_until: new Date(Date.parse('2020-04-29')),
+  // },
+  // {
+  //   issuer: 'did:jolo:aa1bb2cc3dd4',
+  //   details: {
+  //     id_number: 'T3ST0001',
+  //     type: 'A-kaart',
+  //   },
+  //   valid_until: new Date(Date.parse('2020-04-29')),
+  // },
+  // {
+  //   issuer: 'did:jolo:aa1bb2cc3dd4',
+  //   details: {
+  //     id_number: 'T3ST0001',
+  //     type: 'A-kaart',
+  //   },
+  //   valid_until: new Date(Date.parse('2020-04-29')),
+  // },
+  // {
+  //   issuer: 'did:jolo:aa1bb2cc3dd4',
+  //   details: {
+  //     id_number: 'T3ST0001',
+  //     type: 'A-kaart',
+  //   },
+  //   valid_until: new Date(Date.parse('2020-04-29')),
+  // },
   {
     issuer: 'did:jolo:aa1bb2cc3dd4',
     details: {
@@ -49,114 +79,22 @@ const demoDocuments: Document[] = [
     },
     valid_until: new Date(Date.parse('2024-05-14')),
   },
+  {
+    issuer: 'did:jolo:asdqw1321',
+    details: {
+      id_number: 'N0TR34L',
+      type: 'Digital Passport',
+      gender: 'male',
+      birth_place: 'berlin',
+    },
+    valid_until: new Date(Date.parse('2016-03-12')),
+  },
 ]
 
 const debug = {
   // borderColor: 'red',
   // borderWidth: 1,
 }
-
-const documentCardStyles = StyleSheet.create({
-  container: {
-    ...debug,
-    paddingHorizontal: 20,
-    paddingTop: 20,
-    width: '100%',
-  },
-  card: {
-    ...debug,
-    height: 176,
-    backgroundColor: JolocomTheme.primaryColorSand,
-    borderColor: 'rgb(233, 233, 233)',
-    borderWidth: 1,
-    borderRadius: 10,
-    width: '100%',
-    paddingVertical: 16,
-  },
-  documentType: {
-    paddingHorizontal: 15,
-    fontSize: 28,
-    fontFamily: JolocomTheme.contentFontFamily,
-  },
-  documentNumber: {
-    paddingHorizontal: 15,
-    fontSize: 17,
-    fontFamily: JolocomTheme.contentFontFamily,
-  },
-  validityContainer: {
-    flexDirection: 'row',
-    color: 'red',
-    marginTop: 'auto',
-    alignItems: 'center',
-    backgroundColor: 'white',
-    width: '100%',
-    height: 50,
-    paddingHorizontal: 15,
-  },
-  validityText: {
-    marginLeft: 5,
-    fontSize: 17,
-  },
-})
-
-export const DocumentCard: React.SFC<DocumentCardProps> = (
-  props,
-): JSX.Element => (
-  <View style={documentCardStyles.container}>
-    <View style={documentCardStyles.card}>
-      <Text style={documentCardStyles.documentType}>
-        {props.document.details.type}
-      </Text>
-      <Text style={documentCardStyles.documentNumber}>
-        {props.document.details.id_number}
-      </Text>
-      <View style={documentCardStyles.validityContainer}>
-        <Icon size={17} name="check-all" />
-        {props.document.valid_until && (
-          <Text style={documentCardStyles.validityText}>
-            Valid until {props.document.valid_until.toLocaleDateString('en-GB')}
-          </Text>
-        )}
-      </View>
-    </View>
-  </View>
-)
-
-const collapsedDocCardStyles = StyleSheet.create({
-  container: {
-    paddingTop: 20,
-    paddingHorizontal: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  card: {
-    backgroundColor: JolocomTheme.primaryColorWhite,
-    width: 101,
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: 'grey',
-    height: 64,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  icon: {
-    backgroundColor: JolocomTheme.primaryColorGrey,
-    width: 42,
-    height: 42,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-})
-
-export const CollapsedDocumentCard = (props): JSX.Element => (
-  <View style={collapsedDocCardStyles.container}>
-    <View style={collapsedDocCardStyles.card}>
-      <View style={collapsedDocCardStyles.icon}>
-        <Icon size={20} name="checkbox-multiple-blank" />
-      </View>
-    </View>
-  </View>
-)
 
 const issuerCardStyles = StyleSheet.create({
   container: {
@@ -239,6 +177,7 @@ export class InteractionsComponent extends React.Component<Props> {
   public state = {
     activeDocument: 0,
     documentCollapsed: false,
+    showingExpired: false,
   }
 
   private handleScroll = (
@@ -261,7 +200,7 @@ export class InteractionsComponent extends React.Component<Props> {
 
   public render(): JSX.Element {
     const viewWidth: number = Dimensions.get('window').width
-    const { activeDocument } = this.state
+    const { activeDocument, showingExpired } = this.state
 
     return (
       <View style={styles.mainContainer}>
@@ -272,7 +211,7 @@ export class InteractionsComponent extends React.Component<Props> {
             lockScrollWhileSnapping
             lockScrollTimeoutDuration={1000}
             sliderWidth={viewWidth}
-            itemWidth={viewWidth - 30}
+            itemWidth={viewWidth}
             layout={'default'}
             onSnapToItem={(index: number) =>
               this.setState({ activeDocument: index, documentCollapsed: false })
