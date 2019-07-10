91586b0a7575862b5b4bc195c653f01c4f791530
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-07-10 15:53:21 +0200

    add deep link fix

--- index.ts
+++ index.ts
@@ -1,5 +1,18 @@
 import { AppRegistry } from 'react-native'
 
+/**
+ * Object.setPrototypeOf polyfill because typeorm (and possibly others) use it
+ */
+
+// @ts-ignore
+if (!Object.setPrototypeOf) {
+    // @ts-ignore
+    Object.setPrototypeOf = function(obj, proto) {
+        obj.__proto__ = proto
+        return obj
+    }
+}
+
 import App from 'src/App'
 
 AppRegistry.registerComponent('jolocomwallet', () => App)
