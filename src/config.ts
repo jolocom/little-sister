b13f369bd205057db254005bc5f62b2067798058
Mina Nagy Zaki <mnzaki@gmail.com>
2019-08-14 17:24:54 +0200

    use NODE_ENV to configure app
    add babel-plugin-transform-inline-environment-variables
    use process.env['NODE_ENV'] in src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -10,6 +10,13 @@ const typeOrmConf: ConnectionOptions = {
   entities: entityList,
 }
 
+const env = process.env['NODE_ENV'] || 'development'
+const isDev = env === 'development'
+const isTest = env === 'test'
+
+export const skipEntropyCollection = isDev || isTest
+export const skipIdentityRegisteration = isDev || isTest
+
 export default {
   fuelingEndpoint: 'https://faucet.jolocom.com/request',
   typeOrmConfig: typeOrmConf,
