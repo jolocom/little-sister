51e3a2db60801438884fc66e9ec80d9c1a27d05e
Mina Nagy Zaki <mnzaki@gmail.com>
2019-08-22 21:13:56 +0200

    BackendMiddleware: assume control of identityWallet
    Move out backend concerns from the `checkIdentityExists` and
    `createIdentity` redux actions to `BackendMiddleware`
    
    The frontend of the app doesn't need to know about managing the
    `identityWallet`. In general it seems to make sense to keep 'backend'
    concerns contained in `BackendMiddleware`.
    
    - long term overlook ("think global", if this was political)
      - `BackendMiddleware` should deal with all (most?) lib stuff
        - frontend more clearly delineated, and hence (hopefully) easier to test
          - redux actions are 'frontend'!
        - maybe one day `BackendMiddleware` wll move out into its own repo and live on
          as a "build your wallet app" toolkit, with built in jolocom support ;)
      - `prepareIdentityWallet` name just to avoid mental model clashes with previous
        ideas/implementations (`setIdentityWallet` and `createIdentityWallet`)
    - short term actions ("act local", ...)
      - write all new "backend" interactions in `BackendMiddleware` methods
      - add `housekeeping` github issues to move current functionality to
        `BackendMiddleware`

--- src/actions/registration/index.ts
+++ src/actions/registration/index.ts
@@ -2,7 +2,6 @@ import { navigationActions } from 'src/actions/'
 import { routeList } from 'src/routeList'
 import * as loading from 'src/actions/registration/loadingStages'
 import { setDid } from 'src/actions/account'
-import { JolocomLib } from 'jolocom-lib'
 import { generateSecureRandomBytes } from 'src/lib/util'
 import { ThunkAction } from 'src/store'
 
@@ -27,7 +26,6 @@ export const submitEntropy = (
     }),
   )
 
-  dispatch(setLoadingMsg(loading.loadingStages[0]))
   return dispatch(createIdentity(encodedEntropy))
 }
 
@@ -54,7 +52,8 @@ export const createIdentity = (encodedEntropy: string): ThunkAction => async (
   getState,
   backendMiddleware,
 ) => {
-  // This is a just-in-case thing.... maybe multiple button taps or something
+  // This is a just-in-case check.... maybe multiple button taps or a redraw or
+  // something
   const isRegistering = getState().registration.loading.isRegistering
   if (isRegistering) {
     return dispatch(
@@ -66,51 +65,24 @@ export const createIdentity = (encodedEntropy: string): ThunkAction => async (
 
   dispatch(setIsRegistering(true))
 
-  const { encryptionLib, keyChainLib, storageLib, registry } = backendMiddleware
-
-  const password = await keyChainLib.getPassword()
-  const encEntropy = encryptionLib.encryptWithPass({
-    data: encodedEntropy,
-    pass: password,
-  })
-  const entropyData = { encryptedEntropy: encEntropy, timestamp: Date.now() }
-  const userVault = JolocomLib.KeyProvider.fromSeed(
-    Buffer.from(encodedEntropy, 'hex'),
-    password,
-  )
+  dispatch(setLoadingMsg(loading.loadingStages[0]))
+  await backendMiddleware.setEntropy(encodedEntropy)
 
   dispatch(setLoadingMsg(loading.loadingStages[1]))
-
-  await JolocomLib.util.fuelKeyWithEther(
-    userVault.getPublicKey({
-      encryptionPass: password,
-      derivationPath: JolocomLib.KeyTypes.ethereumKey,
-    }),
-  )
+  await backendMiddleware.fuelKeyWithEther()
 
   dispatch(setLoadingMsg(loading.loadingStages[2]))
-  const identityWallet = await registry.create(userVault, password)
+  const identity = await backendMiddleware.createIdentity()
 
-  const personaData = {
-    did: identityWallet.identity.did,
-    controllingKeyPath: JolocomLib.KeyTypes.jolocomIdentityKey,
-  }
-
-  dispatch(setDid(identityWallet.identity.did))
+  dispatch(setDid(identity.did))
   dispatch(setLoadingMsg(loading.loadingStages[3]))
-
-  await storageLib.store.didDoc(identityWallet.didDocument)
-  backendMiddleware.identityWallet = identityWallet
-
-  await storageLib.store.encryptedSeed(entropyData)
-  await storageLib.store.persona(personaData)
-
   dispatch(setIsRegistering(false))
 
+  const mnemonic = bip39.entropyToMnemonic(encodedEntropy)
   return dispatch(
     navigationActions.navigate({
       routeName: routeList.SeedPhrase,
-      params: { mnemonic: bip39.entropyToMnemonic(encodedEntropy) },
+      params: { mnemonic },
     }),
   )
 }
