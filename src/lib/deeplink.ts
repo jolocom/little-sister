88c9c2682de1015ca69be72c746c6a1820c264bd
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-10-23 21:00:49 +0200

    style(deeplink): formatting

--- src/lib/deeplink.ts
+++ src/lib/deeplink.ts
@@ -1,7 +1,12 @@
 
 import { Linking } from 'react-native'
 
-export const respond = (onFinish: () => Promise<any>) => (url: string, token: string) => Linking
-    .canOpenURL(url)
-    .then(can => can ? Linking.openURL(`${url}${token}`).then(onFinish)
-        : Promise.reject('Cant deep link to that url'))
+export const respond = (onFinish: () => Promise<any>) => (
+  url: string,
+  token: string,
+) =>
+  Linking.canOpenURL(url).then(can =>
+    can
+      ? Linking.openURL(`${url}${token}`).then(onFinish)
+      : Promise.reject('Cant deep link to that url'),
+  )
