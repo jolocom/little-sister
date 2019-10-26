9eb0cdef63fcaeef117a9bc5991992b2e4d0c125
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-10-26 21:13:41 +0200

    feat((transport layers)): added basic type for response function

---
+++ src/lib/transportLayers.ts
@@ -0,0 +1,8 @@
+import {
+  JSONWebToken,
+  JWTEncodable,
+} from 'jolocom-lib/js/interactionTokens/JSONWebToken'
+
+type ResponseRoute = string
+
+export type SendResponse = (route: ResponseRoute, token: JSONWebToken<JWTEncodable>) => Promise<boolean>
